b1e01bbad9e26b29f88fbaa1ae2dabeec65760bc
anastasiya-h79 <anastasiya-h79@yandex.ru>
2020-03-07 22:15:23 +0300

    добавила возможность выбирать регион

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from m import parser
 
-
 app = Flask(__name__)
 
 
@@ -10,12 +9,10 @@ def index():
     return render_template('index.html')
 
 
-
 @app.route('/contacts/')
 def contacts():
     developer_name = 'https://github.com/anastasiya-h79/'
     return render_template('contacts.html', name=developer_name)
-
 
 
 @app.route('/request/', methods=['GET'])
@@ -23,13 +20,12 @@ def request_get():
     return render_template('request.html')
 
 
-
 @app.route('/results/', methods=['POST'])
 def request_post():
-    data = request.form['VACANCY']  #and 'AREA'
-    request_result = parser(data)
+    name = request.form['vacancy']
+    area = request.form['area']
+    request_result = parser(name, area)
     return render_template('results.html', data=request_result)
-
 
 
 # @app.route('/results/', methods=['POST'])
@@ -38,7 +34,6 @@ def request_post():
 #         data = json.load(f)
 #     # print(data)
 #     return render_template('results.html', data=data)
-
 
 
 if __name__ == "__main__":
